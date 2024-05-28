776f4868224c358ff83ca31a7a11b5eb21975213
Clyde <curruptdwastaken@gmail.com>
2024-05-28 07:14:56 +0530

    Delete src/ready.js

--- src/ready.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    once: false,
-    run: async (client) => {
-        const allCommands = await client.slashCommands.map(c => c);
-        await client.application.commands.set(allCommands);
-        console.log('[API] Ready to chat!');
-    }
-};
