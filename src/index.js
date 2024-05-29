a7bc342340ea4ce5fb68c6ccddb57be419590a69
Clyde <curruptdwastaken@gmail.com>
2024-05-29 21:37:31 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -36,7 +36,6 @@ client.misc = misc;
         console.log('[CLEVE] Unable to connect: \n' + err);
         return process.exit(1)
     });
-    require('./lavalinkManager')(client); // Load Lavalink manager
 })();
 
 process.on('unhandledRejection', (reason, promise) => {
